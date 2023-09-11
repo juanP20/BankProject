9b5793907ae16761c63b772b4d6aa7450e98b0fe
juan pablo <arenasdurango1999@gmail.com>
2023-09-11 17:24:31 -0500

    fisrt commit

---
+++ src/test/java/com/bank/Bankproject/BankprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.Bankproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
